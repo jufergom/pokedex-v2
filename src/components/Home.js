e3d4a960a3ecb50a0c2a8670b1af7f7ff9f5732c
jufergom <jufergom@hotmail.com>
2020-03-12 01:17:17 -0600

    removed unnecesary paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -131,11 +131,6 @@ export default function Home() {
                     <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
                         Pokedex
                     </Typography>
-                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                    Something short and leading about the collection below—its contents, the creator, etc.
-                    Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-                    entirely.
-                    </Typography>
                 </Container>
                 </div>
                 <Container className={classes.cardGrid} maxWidth="md">
